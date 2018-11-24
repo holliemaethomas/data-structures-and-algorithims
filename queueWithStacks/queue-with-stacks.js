de843e81490e2e833055b933e1439a7b0bb76394
holliemaethomas <holliemaethomas85@gmail.com>
2018-11-23 18:40:31 -0800

    seriously stuck

--- queueWithStacks/queue-with-stacks.js
+++ queueWithStacks/queue-with-stacks.js
@@ -4,4 +4,21 @@ class pseudoQue {
   constructor() {
     this.head = null;
   }
+
+  eneques(value) {
+    let node = new Node(value);
+    node.next = this.prev;
+    this.prev = node;
+    this.length++;
+    return this;
+  }
+
+  dequeue(){
+    let curr = this.prev;
+    while (curr.next || or curr.next.next) {
+      curr = curr.next
+    }
+    let  
+  }
+
 }
