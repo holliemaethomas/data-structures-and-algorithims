6324c36c3de55ee4b502165cfd160eaea1c98836
holliemaethomas <holliemaethomas85@gmail.com>
2018-12-12 20:08:15 -0800

    it works

---
+++ repeatedWord/repeated-word.js
@@ -0,0 +1,15 @@
+let arr = ["a", "b", "a", "d", "a"];
+ function findRepeatWord(arr) {
+  for(var i = 0; i < arr.length; i++){
+  var firstWord = arr[i];
+  for(var j = (i + 1); j < arr.length; j++){
+  var nextWord = arr[j];
+  }
+  }
+  if(firstWord === nextWord)
+    return nextWord;
+ };
+ 
+
+ findRepeatWord(arr);
+
